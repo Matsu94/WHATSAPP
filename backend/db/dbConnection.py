501ca3f753b7c3220763481c1855d912859054d0
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2025-01-20 16:20:46 +0100

    Datos de acceso a la DB

---
+++ backend/db/dbConnection.py
@@ -0,0 +1,18 @@
+import pymysql.cursors
+import sqlalchemy as db
+
+class Chat(object):
+    def conecta(self):
+        self.db = pymysql.connect(
+            host="192.168.193.133:3306",
+            user="matiasianbastero",
+            password="49864854A",
+            db="matias",
+            charset="utf8mb4",
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cursor = self.db.cursor()
+
+    def desconecta(self):
+        self.db.close()
