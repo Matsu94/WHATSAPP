3552117f58a21526a24543c77d3e8b7ac5eaf3b8
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2025-01-23 15:43:49 +0100

    integración código db en controllers

--- backend/controllers/controllers.py
+++ backend/controllers/controllers.py
@@ -1,3 +1,31 @@
+import pymysql.cursors
+
+class Matias(object):
+    def conecta(self):
+        self.db = pymysql.connect(
+            host="192.168.193.133:3306",
+            user="matiasianbastero",
+            password="49864854A",
+            db="matias",
+            charset="utf8mb4",
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cursor = self.db.cursor()
+
+    def desconecta(self):
+        self.db.close()
+        
+    
+
+def get_db():
+    db = Matias()
+    try:
+        db.conecta()
+        yield db
+    finally:
+        db.desconecta()
+        
 # Query to send a message
 def sendMessage(self, message):
     sql = "INSERT INTO messages (content, date, status, sender, receiver) VALUES (%s, %s, %s, %s, %s)"
