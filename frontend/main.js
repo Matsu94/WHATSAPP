610edb7e394088411248e3d07c56ebda341fd9ef
Daniel Ramon Campos <danielramon@paucasesnovescifp.cat>
2025-02-03 19:40:19 +0100

    menu desplegable

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,3 @@
-import { fetchUsers } from "./assets/fetching.js";
 import { renderUserList } from "./utils/renderUserList.js";
 import { searchUsers } from "./utils/searchUsers.js";
 import { openCreateGroupForm } from "./utils/openCreateGroupForm.js";
@@ -7,30 +6,40 @@ import { fetchChats } from "./assets/fetching.js";
 
 // Inicialización
 window.addEventListener("DOMContentLoaded", () => {
-    async function init() {
-      try {
-        const chats = await fetchChats();
-            renderUserList(chats);
-        const searchInput = document.getElementById("searchInput");
-        if (searchInput) {
-          searchInput.addEventListener("input", (e) => {
-            const filtered = searchUsers(chats, e.target.value);
-            renderUserList(filtered);
-          });
-        }
-  
-        const createGroupBtn = document.getElementById("createGroupBtn");
-  if (createGroupBtn) {
-    createGroupBtn.addEventListener("click", () => {
-      openCreateGroupForm();
-    });
-  }
-  
-      } catch (error) {
-        console.error(getUsersError, error);
-        // Manejo de error si deseas
+  async function init() {
+    try {
+      const chats = await fetchChats();
+      renderUserList(chats);
+
+      const searchInput = document.getElementById("searchInput");
+      if (searchInput) {
+        searchInput.addEventListener("input", (e) => {
+          const filtered = searchUsers(chats, e.target.value);
+          renderUserList(filtered);
+        });
       }
+
+      const createGroupBtn = document.getElementById("createGroupBtn");
+      if (createGroupBtn) {
+        createGroupBtn.addEventListener("click", () => {
+          openCreateGroupForm();
+        });
+      }
+
+      // Menú desplegable
+      const menuBtn = document.getElementById('menuBtn');
+      const dropdownMenu = document.getElementById('dropdownMenu');
+
+      // Abrir o cerrar el menú al hacer clic en el botón
+      menuBtn.addEventListener('click', () => {
+          dropdownMenu.classList.toggle('hidden'); // Alternar la clase 'hidden'
+      });
+
+    } catch (error) {
+      console.error(getUsersError, error);
+      // Manejo de error si deseas
     }
-  
-    init(); 
-  });
+  }
+
+  init();
+});
