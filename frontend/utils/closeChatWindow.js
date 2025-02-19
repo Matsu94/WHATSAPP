dac76d552ea7d74b32ada9a567fc9c3851c7e77f
Matsu94 <matuszdx@gmail.com>
2025-02-19 03:04:59 +0100

    Implemented ws for user-user and groups. main.py - ws implementation, and changes to sendMessage openChat.js - created closeWebToken and onmessage implementation renderChatMessages.js - implemented append logic.

--- frontend/utils/closeChatWindow.js
+++ frontend/utils/closeChatWindow.js
@@ -1,12 +1,13 @@
+import { closeWebSocket } from "./openChat.js"; // Import the WebSocket cleanup function
+
 // Cerrar chat
 export function closeChatWindow() {
-
-  const chatWindow = document.getElementById("chatWindow");
-  if (!chatWindow) return;
-  chatWindow.innerHTML = `
-    <p class="text-xl text-center px-4">
-      Pulsa en una conversación para ver los mensajes
-    </p>
-  `;  
+    const chatWindow = document.getElementById("chatWindow");
+    if (!chatWindow) return;
+    chatWindow.innerHTML = `
+      <p class="text-xl text-center px-4">
+        Pulsa en una conversación para ver los mensajes
+      </p>
+    `;
+    closeWebSocket(); // Close the WebSocket connection
 }
-
--- frontend/utils/openChat.js
+++ frontend/utils/openChat.js
@@ -7,93 +7,118 @@ import { renderUserList } from "./renderUserList.js";
 import { openGroupOptions } from "./openGroupOptions.js";
 import { initScrollPagination, resetPagination } from "./scrolling.js";
 import { searchChats } from "./searchUsers.js";
+import { renderChatMessages } from "./renderChatMessages.js";
 
 let socket = null;
 
 export async function openChat(senderId, isGroup, senderName) {
-  const chatWindow = document.getElementById("chatWindow");
-  if (!chatWindow) return;
+    closeWebSocket();
+    const chatWindow = document.getElementById("chatWindow");
+    if (!chatWindow) return;
 
-  // Load messages first before updating chat list
-  await fetchMessages(senderId, isGroup, 0);
+    // Load messages first before updating chat list so alerts are deleted
+    await fetchMessages(senderId, isGroup, 0);
 
-  const chats = await fetchChats();
-  renderUserList(chats);
+    const chats = await fetchChats();
+    renderUserList(chats);
 
-  const searchInput = document.getElementById("searchInput");
-  if (searchInput) {
-    searchInput.addEventListener("input", (e) => {
-      const filtered = searchChats(chats, e.target.value);
-      renderUserList(filtered);
-    });
-  }
-
-  // Cargar el contenido de openChat.html
-  fetch("/WHATSAPP/frontend/components/openChat.html")
-    .then((response) => response.text())
-    .then((html) => {
-      chatWindow.innerHTML = html;
-
-      const chatHeader = document.getElementById("chatHeader");
-      const groupOptions = document.getElementById("groupOptions");
-      if (chatHeader) {
-        chatHeader.textContent = senderName; // Default header text
-      }
-
-      if (isGroup && groupOptions) {
-        groupOptions.classList.remove("hidden");
-        groupOptions.addEventListener("click", () => {
-          openGroupOptions(senderId);
+    const searchInput = document.getElementById("searchInput");
+    if (searchInput) {
+        searchInput.addEventListener("input", (e) => {
+            const filtered = searchChats(chats, e.target.value);
+            renderUserList(filtered);
         });
-      }
+    }
 
-      // Cargar y mostrar los mensajes
-      loadMessages(senderId, isGroup);
-      resetPagination();
-      initScrollPagination(senderId, isGroup);
+    // Cargar el contenido de openChat.html
+    fetch("/WHATSAPP/frontend/components/openChat.html")
+        .then((response) => response.text())
+        .then((html) => {
+            chatWindow.innerHTML = html;
 
-      // Agregar eventos a los elementos
-      const sendBtn = document.getElementById("sendMessageBtn");
-      const input = document.getElementById("newMessageInput");
+            const chatHeader = document.getElementById("chatHeader");
+            const groupOptions = document.getElementById("groupOptions");
+            if (chatHeader) {
+                chatHeader.textContent = senderName; // Default header text
+            }
 
-      sendBtn.addEventListener("click", async () => {
-        sendMessage(senderId, isGroup);
-        const chats = await fetchChats();
-        renderUserList(chats);
-      });
+            if (isGroup && groupOptions) {
+                groupOptions.classList.remove("hidden");
+                groupOptions.addEventListener("click", () => {
+                    openGroupOptions(senderId);
+                });
+            }
 
-      input.addEventListener("keydown", async (e) => {
-        if (e.key === "Enter") {
-          sendMessage(senderId, isGroup);
-          const chats = await fetchChats();
-          renderUserList(chats);
-        } else if (e.key === "Escape") {
-          closeChatWindow();
-        }
-      });
+            // Cargar y mostrar los mensajes
+            loadMessages(senderId, isGroup);
+            resetPagination();
+            initScrollPagination(senderId, isGroup);
 
-      // Esperar a que el DOM actualice y luego llamar la función
-      setTimeout(() => {
-        const closeBtn = document.getElementById("closeChatBtn");
-        const chatList = document.getElementById("chatList");
-        const userListDiv = document.getElementById("userListDiv");
-        const chatWindow = document.getElementById("chatWindow");
+            // Establecer conexión WebSocket
+            const currentUserId = sessionStorage.getItem('user_id');
+            const roomId = isGroup ? ("Group_"+senderId) : currentUserId;
+            socket = new WebSocket(`ws://127.0.0.1:8000/ws/${roomId}`);
+            
+            socket.onmessage = (event) => {
+                const data = JSON.parse(event.data);  // Parse the JSON array
+                if (Array.isArray(data)) {
+                    renderChatMessages(data, { append: true });
+                } else {
+                    console.error("Invalid message format:", data);
+                }
+                // loadMessages(senderId, isGroup);
+            };
 
-        if (closeBtn) {
-          closeBtn.addEventListener("click", () => {
-            closeChatWindow();
-            chatList.classList.remove("hidden");
-            chatWindow.classList.add("hidden");
-            userListDiv.classList.remove("hidden");
-            closeBtn.classList.add("hidden");
-          });
-        }
-      }, 100);
+            // Agregar eventos a los elementos
+            const sendBtn = document.getElementById("sendMessageBtn");
+            const input = document.getElementById("newMessageInput");
 
-      // Poner foco en el input
-      input.focus();
-    })
-    .catch((err) => {
-      console.error(`${openChatError}`, err);
-    });
+            sendBtn.addEventListener("click", async () => {
+                sendMessage(senderId, isGroup);
+                const chats = await fetchChats();
+                renderUserList(chats);
+            });
+
+            input.addEventListener("keydown", async (e) => {
+                if (e.key === "Enter") {
+                    sendMessage(senderId, isGroup);
+                    const chats = await fetchChats();
+                    renderUserList(chats);
+                } else if (e.key === "Escape") {
+                    closeChatWindow();
+                }
+            });
+
+            // Esperar a que el DOM actualice y luego llamar la función
+            setTimeout(() => {
+                const closeBtn = document.getElementById("closeChatBtn");
+                const chatList = document.getElementById("chatList");
+                const userListDiv = document.getElementById("userListDiv");
+                const chatWindow = document.getElementById("chatWindow");
+
+                if (closeBtn) {
+                    closeBtn.addEventListener("click", () => {
+                        closeChatWindow();
+                        chatList.classList.remove("hidden");
+                        chatWindow.classList.add("hidden");
+                        userListDiv.classList.remove("hidden");
+                        closeBtn.classList.add("hidden");
+                    });
+                }
+            }, 100);
+
+            // Poner foco en el input
+            input.focus();
+        })
+        .catch((err) => {
+            console.error(`${openChatError}`, err);
+        });
+}
+
+// Close WebSocket connection when chat window is closed
+export function closeWebSocket() {
+    if (socket) {
+        socket.close();
+        socket = null;
+    }
 }
