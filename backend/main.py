acf5d1815ca9b6a080fb08366a8a2916881dc3b0
Matsu94 <matuszdx@gmail.com>
2025-03-23 18:08:28 +0100

    Changed import and db connection

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import Body, FastAPI, Depends, HTTPException, status, WebSocket, WebSocketDisconnect
-from controllers.jwt_auth_users import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token, get_current_user
+from controllers.jwt_auth_users import *
 from controllers.controllers import Matias
 from models.models import *
 from datetime import timedelta
